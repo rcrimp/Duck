2b3ca3eeb26625e5f636f31cbb5a6c189d52c025
Reuben <reuben@macaroni.otago.ac.nz>
2017-05-25 11:09:23 +1200

    updated the project homepage

---
+++ project/index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>The Duck</title>
+	</head>
+	<body>
+		<h1>The Duck</h1>
+		<a href="https://docs.google.com/spreadsheets/d/1Ht3jQCUXiCbsIsW3VAqD8J8kjFPAZB7ApPuJrEOBHhY/edit?usp=sharing">Google docs account</a>
+		<p>Why did the AI android children misbehave?<br>because they were trained with unsupervised algorithms<p>
+
+		<p>Why were the CS students allowed to copy off eachother during their exam?<br>Because all information inside a class is publically accessible</p>
+
+		<p>I like my relationships the way I like my source code... Open</p>
+	</body>
+</html>
